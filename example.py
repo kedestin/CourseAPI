6fae14023d55eb7390d2a93bc7a24c084b86ab71
kedestin <26238343+kedestin@users.noreply.github.com>
2020-05-11 04:53:47 -0400

    Search results have different schema

--- example.py
+++ example.py
@@ -1,27 +1,37 @@
-import tufts_courses as tufts
+import src.tufts_courses as tufts
 import json
-
-"""
-Pretty print
-"""
 
 
 def pprint(obj):
+    """
+    Pretty print
+    """
     print(json.dumps(obj, indent=2))
 
 
 def main():
     career = tufts.getCareer()[0]
     term = tufts.getTerms(career=career["value"])[0]
-    subject = tufts.getSubjects(
-        career=career["value"], term=term["value"])
+    subject = tufts.getSubjects(career=career["value"], term=term["value"])
     toFind = subject[10]
-    print("Results for search with parameters: ")
-    pprint({"career": career["value"],
-            "term": term["value"], "subject": toFind["desc"]})
 
-    pprint(tufts.performSearch(
-        career=career["value"], term=term["value"], subject=toFind["value"])[0])
+    print("Performing search")
+    pprint(
+        {
+            "career": career["value"],
+            "term": term["value"],
+            "subject": toFind["desc"]
+        }
+    )
+
+    print("\nFirst result:")
+    pprint(
+        tufts.performSearch(
+            career=career["value"],
+            term=term["value"],
+            subject=toFind["value"]
+        )["searchResults"][0]
+    )
 
 
 if __name__ == "__main__":
